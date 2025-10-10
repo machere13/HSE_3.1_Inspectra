1f866b3bb5f284a83ccdb6db48e71c917e9ee7cf
machere13 <vladislav.chubarov13@yandex.ru>
2025-10-10 11:27:21 +0300

    feat: added routing

--- config/routes.rb
+++ config/routes.rb
@@ -23,12 +23,16 @@ Rails.application.routes.draw do
     end
   end
 
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  
+  resources :days, only: [:show] do
+    resources :articles, only: [:show]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
