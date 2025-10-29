26f0278fb7557c54ed1e5550db99c0bab7fad2a0
machere13 <vladislav.chubarov13@yandex.ru>
2025-10-29 12:33:05 +0300

    feat: added PageNotFound

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,12 @@
+class ErrorsController < WebController
+  layout 'plug'
+  def not_found
+    respond_to do |format|
+      format.html { render 'errors/not_found', status: :not_found }
+      format.json { render json: { error: 'Not Found' }, status: :not_found }
+      format.all  { render plain: '404 Not Found', status: :not_found }
+    end
+  end
+end
+
+
