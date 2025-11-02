550358217751d07e67e9dd6ac74df11fdd8a109e
machere13 <vladislav.chubarov13@yandex.ru>
2025-11-02 19:28:35 +0300

    refactor: structured json logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,29 @@
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
+  :passw,
+  :password,
+  :password_confirmation,
+  :email,
+  :secret,
+  :secret_key_base,
+  :token,
+  :jwt,
+  :jti,
+  :_key,
+  :crypt,
+  :salt,
+  :certificate,
+  :otp,
+  :verification_code,
+  :reset_password_token,
+  :ssn,
+  :cvv,
+  :cvc,
+  :authorization,
+  :authenticity_token,
+  :api_key,
+  :api_secret,
+  :access_token,
+  :refresh_token
 ]
