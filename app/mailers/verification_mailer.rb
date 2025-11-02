86ba4a759eaf00356474a2e2e9a4ec5667ecbaf8
machere13 <vladislav.chubarov13@yandex.ru>
2025-11-02 18:39:29 +0300

    fix: dynamic config choose

--- app/mailers/verification_mailer.rb
+++ app/mailers/verification_mailer.rb
@@ -2,10 +2,11 @@ class VerificationMailer < ApplicationMailer
   def send_verification_code(user)
     @user = user
     @verification_code = user.verification_code
-    smtp_config = SmtpConfigService.get_smtp_config(@user.email)
+    sender_email = ENV['DEFAULT_EMAIL_USERNAME']
+    smtp_config = SmtpConfigService.get_smtp_config(sender_email)
 
     mail(
-      from: ENV['DEFAULT_EMAIL_USERNAME'],
+      from: sender_email,
       to: @user.email, 
       subject: 'Код подтверждения',
       delivery_method_options: smtp_config
