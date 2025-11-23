eb8ebf45f92eabca449147bd58ab06cd71b37e74
machere13 <vladislav.chubarov13@yandex.ru>
2025-11-24 00:11:14 +0300

    fixed: admin week detail page

--- app/controllers/admin/weeks_controller.rb
+++ app/controllers/admin/weeks_controller.rb
@@ -1,18 +1,22 @@
 class Admin::WeeksController < Admin::BaseController
-  load_and_authorize_resource
+  before_action :set_week, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_week_access, only: [:show, :edit, :update, :destroy]
 
   def index
     @pagy, @weeks = pagy(Week.order(:number), items: 20)
+    authorize! :read, Week
   end
 
   def show; end
 
   def new
     @week = Week.new
+    authorize! :create, @week
   end
 
   def create
     @week = Week.new(week_params)
+    authorize! :create, @week
     if @week.save
       redirect_to admin_week_path(@week), notice: 'Неделя создана'
     else
@@ -38,6 +42,17 @@ class Admin::WeeksController < Admin::BaseController
   end
 
   private
+
+  def set_week
+    id = params[:id].to_s
+    @week = Week.find_by(number: id.to_i) || Week.find_by(number: id) || Week.find_by(id: id)
+    raise ActiveRecord::RecordNotFound unless @week
+  end
+
+  def authorize_week_access
+    authorize! :read, @week
+    authorize! :update, @week if ['edit', 'update', 'destroy'].include?(action_name)
+  end
 
   def week_params
     params.require(:week).permit(:number, :title, :description, :published_at, :expires_at)
