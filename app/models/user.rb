3f283ac505c6ddf48d8f5e286d1726d497ecb1f5
machere13 <vladislav.chubarov13@yandex.ru>
2025-12-07 23:05:51 +0300

    feat: added nickname generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: AppConfig::Auth.password_min_length, maximum: AppConfig::Auth.password_max_length }, if: -> { new_record? || !password.nil? }
   
+  before_validation :generate_name_if_blank, on: :create
   after_create :check_registration_achievements
   
   def generate_verification_code!
@@ -98,5 +99,53 @@ class User < ApplicationRecord
   def reset_token_valid?(ttl_minutes: nil)
     ttl = ttl_minutes || AppConfig::Auth.reset_password_token_ttl_minutes.to_i / 60
     reset_password_token.present? && reset_password_sent_at.present? && reset_password_sent_at > ttl.minutes.ago
+  end
+
+  def generate_code_metaphor_name!
+    loop do
+      generated_name = generate_code_metaphor_name
+      unless User.exists?(name: generated_name)
+        self.name = generated_name
+        break
+      end
+    end
+  end
+
+  private
+
+  def generate_name_if_blank
+    return if name.present?
+    generate_code_metaphor_name!
+  end
+
+  def generate_code_metaphor_name
+    prefixes = [
+      'Code', 'Bug', 'Syntax', 'Debug', 'Byte', 'Bit', 'Stack', 'Heap', 'Queue', 'Tree',
+      'Node', 'Array', 'Hash', 'List', 'Set', 'Map', 'Graph', 'Loop', 'Recursion', 'Lambda',
+      'Function', 'Class', 'Object', 'Method', 'Variable', 'Constant', 'String', 'Integer',
+      'Float', 'Boolean', 'Null', 'Void', 'Return', 'Break', 'Continue', 'Async', 'Promise',
+      'Callback', 'Event', 'Stream', 'Buffer', 'Cache', 'Cookie', 'Session', 'Token', 'Key',
+      'Value', 'Index', 'Pointer', 'Reference', 'Instance', 'Static', 'Public', 'Private',
+      'Protected', 'Abstract', 'Interface', 'Trait', 'Module', 'Package', 'Import', 'Export',
+      'Require', 'Include', 'Extend', 'Inherit', 'Override', 'Polymorph', 'Encapsulate',
+      'Abstraction', 'Inheritance', 'Composition', 'Aggregation', 'Dependency', 'Association'
+    ]
+    
+    suffixes = [
+      'Ninja', 'Master', 'Wizard', 'Hunter', 'Warrior', 'Knight', 'Guardian', 'Defender',
+      'Builder', 'Creator', 'Maker', 'Designer', 'Architect', 'Engineer', 'Developer',
+      'Coder', 'Programmer', 'Hacker', 'Guru', 'Expert', 'Pro', 'Elite', 'Legend',
+      'Hero', 'Champion', 'Ace', 'Star', 'Genius', 'Savant', 'Virtuoso', 'Maestro',
+      'Pioneer', 'Trailblazer', 'Innovator', 'Visionary', 'Strategist', 'Tactician',
+      'Solver', 'Fixer', 'Optimizer', 'Refactorer', 'Debugger', 'Tester', 'Validator',
+      'Executor', 'Runner', 'Processor', 'Handler', 'Manager', 'Controller', 'Router',
+      'Service', 'Repository', 'Factory', 'Builder', 'Singleton', 'Observer', 'Mediator'
+    ]
+    
+    prefix = prefixes.sample
+    suffix = suffixes.sample
+    number = rand(100..9999)
+    
+    "#{prefix}#{suffix}#{number}"
   end
 end
