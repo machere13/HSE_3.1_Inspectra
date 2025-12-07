4fefc75acc8d2f38dd6801c05acb87b9ed97021b
machere13 <vladislav.chubarov13@yandex.ru>
2025-12-07 23:07:40 +0300

    feat: decomposed prefixes and suffixes in generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,29 @@ class User < ApplicationRecord
     super_admin: 3
   }
   
+  CODE_METAPHOR_PREFIXES = [
+    'Code', 'Bug', 'Syntax', 'Debug', 'Byte', 'Bit', 'Stack', 'Heap', 'Queue', 'Tree',
+    'Node', 'Array', 'Hash', 'List', 'Set', 'Map', 'Graph', 'Loop', 'Recursion', 'Lambda',
+    'Function', 'Class', 'Object', 'Method', 'Variable', 'Constant', 'String', 'Integer',
+    'Float', 'Boolean', 'Null', 'Void', 'Return', 'Break', 'Continue', 'Async', 'Promise',
+    'Callback', 'Event', 'Stream', 'Buffer', 'Cache', 'Cookie', 'Session', 'Token', 'Key',
+    'Value', 'Index', 'Pointer', 'Reference', 'Instance', 'Static', 'Public', 'Private',
+    'Protected', 'Abstract', 'Interface', 'Trait', 'Module', 'Package', 'Import', 'Export',
+    'Require', 'Include', 'Extend', 'Inherit', 'Override', 'Polymorph', 'Encapsulate',
+    'Abstraction', 'Inheritance', 'Composition', 'Aggregation', 'Dependency', 'Association'
+  ].freeze
+
+  CODE_METAPHOR_SUFFIXES = [
+    'Ninja', 'Master', 'Wizard', 'Hunter', 'Warrior', 'Knight', 'Guardian', 'Defender',
+    'Builder', 'Creator', 'Maker', 'Designer', 'Architect', 'Engineer', 'Developer',
+    'Coder', 'Programmer', 'Hacker', 'Guru', 'Expert', 'Pro', 'Elite', 'Legend',
+    'Hero', 'Champion', 'Ace', 'Star', 'Genius', 'Savant', 'Virtuoso', 'Maestro',
+    'Pioneer', 'Trailblazer', 'Innovator', 'Visionary', 'Strategist', 'Tactician',
+    'Solver', 'Fixer', 'Optimizer', 'Refactorer', 'Debugger', 'Tester', 'Validator',
+    'Executor', 'Runner', 'Processor', 'Handler', 'Manager', 'Controller', 'Router',
+    'Service', 'Repository', 'Factory', 'Builder', 'Singleton', 'Observer', 'Mediator'
+  ].freeze
+
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: AppConfig::Auth.password_min_length, maximum: AppConfig::Auth.password_max_length }, if: -> { new_record? || !password.nil? }
   
@@ -119,31 +142,8 @@ class User < ApplicationRecord
   end
 
   def generate_code_metaphor_name
-    prefixes = [
-      'Code', 'Bug', 'Syntax', 'Debug', 'Byte', 'Bit', 'Stack', 'Heap', 'Queue', 'Tree',
-      'Node', 'Array', 'Hash', 'List', 'Set', 'Map', 'Graph', 'Loop', 'Recursion', 'Lambda',
-      'Function', 'Class', 'Object', 'Method', 'Variable', 'Constant', 'String', 'Integer',
-      'Float', 'Boolean', 'Null', 'Void', 'Return', 'Break', 'Continue', 'Async', 'Promise',
-      'Callback', 'Event', 'Stream', 'Buffer', 'Cache', 'Cookie', 'Session', 'Token', 'Key',
-      'Value', 'Index', 'Pointer', 'Reference', 'Instance', 'Static', 'Public', 'Private',
-      'Protected', 'Abstract', 'Interface', 'Trait', 'Module', 'Package', 'Import', 'Export',
-      'Require', 'Include', 'Extend', 'Inherit', 'Override', 'Polymorph', 'Encapsulate',
-      'Abstraction', 'Inheritance', 'Composition', 'Aggregation', 'Dependency', 'Association'
-    ]
-    
-    suffixes = [
-      'Ninja', 'Master', 'Wizard', 'Hunter', 'Warrior', 'Knight', 'Guardian', 'Defender',
-      'Builder', 'Creator', 'Maker', 'Designer', 'Architect', 'Engineer', 'Developer',
-      'Coder', 'Programmer', 'Hacker', 'Guru', 'Expert', 'Pro', 'Elite', 'Legend',
-      'Hero', 'Champion', 'Ace', 'Star', 'Genius', 'Savant', 'Virtuoso', 'Maestro',
-      'Pioneer', 'Trailblazer', 'Innovator', 'Visionary', 'Strategist', 'Tactician',
-      'Solver', 'Fixer', 'Optimizer', 'Refactorer', 'Debugger', 'Tester', 'Validator',
-      'Executor', 'Runner', 'Processor', 'Handler', 'Manager', 'Controller', 'Router',
-      'Service', 'Repository', 'Factory', 'Builder', 'Singleton', 'Observer', 'Mediator'
-    ]
-    
-    prefix = prefixes.sample
-    suffix = suffixes.sample
+    prefix = CODE_METAPHOR_PREFIXES.sample
+    suffix = CODE_METAPHOR_SUFFIXES.sample
     number = rand(100..9999)
     
     "#{prefix}#{suffix}#{number}"
