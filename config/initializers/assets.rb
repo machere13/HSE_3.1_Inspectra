d83f68ff2edb1062357345f012f8f1d3820a6592
machere13 <vladislav.chubarov13@yandex.ru>
2025-12-24 14:47:37 +0300

    feat: added O_Breadcrumbs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,7 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'icons
 Rails.application.config.assets.precompile += %w[
   auth/verify.js
 ]
+
+Rails.application.config.assets.precompile << Proc.new do |path|
+  path.start_with?(Rails.root.join('app', 'assets', 'icons').to_s) && path.end_with?('.svg')
+end
