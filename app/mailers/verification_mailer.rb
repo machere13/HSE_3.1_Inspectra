65c5182281737cadbcc5760ea00190fcf3f2594b
machere13 <vladislav.chubarov13@yandex.ru>
2026-04-20 21:03:11 +0300

    feat: added code mailer

--- app/mailers/verification_mailer.rb
+++ app/mailers/verification_mailer.rb
@@ -5,6 +5,13 @@ class VerificationMailer < ApplicationMailer
     sender_email = ENV['DEFAULT_EMAIL_USERNAME']
     smtp_config = SmtpConfigService.get_smtp_config(sender_email)
 
+    logo_path = Rails.root.join('app/assets/images/mailers/Logo.png')
+    attachments.inline['logo.png'] = {
+      mime_type: 'image/png',
+      content: File.binread(logo_path),
+      content_id: 'inspectra-logo'
+    }
+
     mail(
       from: sender_email,
       to: @user.email, 
